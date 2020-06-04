4ff13ba6c6d12bb56a771fea8515a4935c7c3c08
Dana L. Brown <dana.l.brown@gmail.com>
2020-06-04 14:36:51 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//importing the main app component
+import App from './App';
+
+// this is app.js
+ReactDOM.render (<App />, document.getElementById('root'));
+
+
